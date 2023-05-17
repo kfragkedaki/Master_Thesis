227d36862c53fdd956f182d677a26831bdd7f346
fragkedaki <kleio.fragkedaki@einride.tech>
2023-05-17 22:24:51 +0200

    add plot_heatmaps to log_utils

--- src/utils/log_utils.py
+++ src/utils/log_utils.py
@@ -1,6 +1,43 @@
 from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-writer = SummaryWriter(log_dir="logs")
+from datetime import datetime
+import torch
+
+from sklearn.manifold import TSNE
+from sklearn.metrics.pairwise import cosine_similarity
+
+logdir = "logs/plots/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+writer = SummaryWriter(log_dir=logdir)
+
+
+def plot_heatmaps(figures, step=0, title="Heatmap Figures"):
+    fig, axs = plt.subplots(1, len(figures), layout="constrained")
+
+    # Create the heatmap
+    img = None
+    for index, (name, figure) in enumerate(figures.items()):
+        img = axs[index].imshow(
+            figure, cmap="hot", interpolation="nearest"
+        )
+        axs[index].set_title(name + ' ' + str(step))
+
+    fig.colorbar(img, shrink=0.6)
+
+    # # Convert the figure to an image
+    # Convert the figure to a numpy array
+    fig.canvas.draw()
+    image = np.array(fig.canvas.renderer.buffer_rgba())
+
+    # Convert the image to a torch tensor
+    image_tensor = torch.from_numpy(image).permute(2, 0, 1).contiguous()
+
+    # Write the image to TensorBoard
+    writer.add_image(title, image_tensor, global_step=step)
+
+    plt.show()
 
 
 def log_values(
@@ -57,3 +94,76 @@ def log_values(
                 writer.add_histogram(
                     f"Parameter: {name}", param.clone().cpu().data.numpy(), epoch
                 )
+
+        encoder_distance = {
+            'input_distance': (model.encoder_data['input'][0, :, None, :] - model.encoder_data['input'][0, None, :, :]).norm(p=2, dim=-1),
+            'embedding_distance': (
+                        model.encoder_data['embeddings'][0, :, None, :] - model.encoder_data['embeddings'][0, None, :, :]).norm(p=2,
+                                                                                                                  dim=-1)
+        }
+
+        # Compute cosine similarity between embeddings
+        # only the first instance in the batch_size
+        encoder_cosine = {
+            'input_cos': torch.from_numpy(
+                cosine_similarity(model.encoder_data['input'][0].numpy())),
+            'embeddings_cos': torch.from_numpy(
+                cosine_similarity(model.encoder_data['embeddings'][0].numpy()))
+        }
+
+        plot_heatmaps(encoder_distance, step)
+        plot_heatmaps(encoder_cosine, step, title="Heatmap Figures 2")
+
+
+# # Create the heatmap
+# img1 = axs[0].imshow(
+#     input_distance.cpu().detach().numpy(), cmap="hot", interpolation="nearest"
+# )
+# axs[0].set_title(f"input_distance_np ${step}")
+#
+# # Create the heatmap
+# img2 = axs[1].imshow(
+#     embeddings_distance.cpu().detach().numpy(), cmap="hot", interpolation="nearest"
+# )
+# axs[1].set_title(f"embeddings_distance_np ${step}")
+# fig.colorbar(img2, ax=axs[1], shrink=0.6)
+#
+# # Convert the figure to an image
+# fig.canvas.draw()
+# image = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+# image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+# image = np.expand_dims(image, 0)
+#
+# # Convert the image to a TensorFlow tensor
+# image_tensor = tf.image.convert_image_dtype(image, dtype=tf.uint8)
+#
+# # Write the image to TensorBoard
+# with writer.as_default():
+#     tf.summary.image(f"Heatmap Figures", image_tensor, step=step)
+
+
+# tf.summary.image("Training data", plot_to_image(fig), step=0)
+
+#
+# # If you want to visualize the cosine similarity as a heatmap
+# plt.figure(figsize=(10, 10))
+# plt.imshow(cos_sim, cmap='hot', interpolation="nearest")
+# plt.title("Cosine similarity heatmap of the embeddings")
+# plt.colorbar()
+# plt.show()
+
+# # Use t-SNE to reduce dimensionality to 2D for visualization
+# embeddings_first_graph = embeddings[0].cpu().detach().numpy()
+# tsne = TSNE(n_components=2, perplexity=embeddings_first_graph.shape[0] - 1)
+# embeddings_2d = tsne.fit_transform(embeddings_first_graph)
+#
+# # Plot the 2D embeddings
+# plt.figure(figsize=(8, 6))
+# plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1])
+# plt.title("2D visualization of the embeddings using t-SNE")
+# plt.show()
+#
+# plt.figure(figsize=(8, 6))
+# plt.scatter(input[0][:, 0], input[0][:, 1])
+# plt.title("2D visualization of the embeddings using t-SNE")
+# plt.show()
