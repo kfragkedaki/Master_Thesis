222c7486deec24a94fafea2c402b4e2e485b8bbe
fragkedaki <kleio.fragkedaki@einride.tech>
2023-08-04 19:22:24 +0300

    save images and fix colours

--- src/utils/plot_timeline.py
+++ src/utils/plot_timeline.py
@@ -3,9 +3,26 @@ import torch
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import seaborn as sns
 
 
-def plot_timeline(data: np.array = []) -> None:
+def get_trailer_colors(size, type="matplotlib"):
+    set3 = plt.get_cmap('Set3')
+    colors = [set3(i) for i in range(size+1)]
+    if size > 1:
+        colors[1] = set3(5)  # Replace the 2nd color with the 6th one
+
+    if type == "plotly":
+        colors = px.colors.qualitative.Set3[: size+1]
+        if size > 1:
+            colors[1] = px.colors.qualitative.Set3[5]  # Replace the 2nd color with the 6th one
+
+    return colors
+
+
+def plot_timeline(data: np.array = [], example_name="example") -> None:
     assert isinstance(data, np.ndarray) or (
         len(data) > 0 and len(data[0]) > 0
     ), "Wrong data type"
@@ -29,6 +46,8 @@ def plot_timeline(data: np.array = []) -> None:
         if trailer not in trailer_names:
             trailer_names.append(trailer)
 
+        trailer_names = sorted(trailer_names, key=lambda x: int(x.split()[1]) if x is not None and x.split()[1]!='None' else float('inf'))
+
         block = dict(
             Truck=truck,
             StartTime=int(time),
@@ -42,7 +61,7 @@ def plot_timeline(data: np.array = []) -> None:
     fig = go.Figure()
 
     # Generate a categorical color scale based on the unique trailer names
-    trailer_colors = px.colors.qualitative.Set3[: len(trailer_names)]
+    trailer_colors = get_trailer_colors(len(trailer_names), type="plotly")
     y_data = []
     for truck, truck_df in df.groupby("Truck"):
         for idx, row in truck_df.iterrows():
@@ -120,10 +139,11 @@ def plot_timeline(data: np.array = []) -> None:
             tickvals=df["Truck"].unique(),
             ticktext=y_data),
         margin=dict(pad=5),
-        legend=dict(traceorder="reversed"), showlegend=True, width=base_size * len(df), height=1.25*base_size * (len(df["Truck"].unique())+1), autosize=False)
+        legend=dict(traceorder="normal"), showlegend=True, width=base_size * len(df), height=1.25*base_size * (len(df["Truck"].unique())+1))
     fig.update_xaxes(title="Steps")
     fig.update_yaxes(autorange="reversed")  # Tasks listed from top to bottom
     fig.show()
+    fig.write_image(f"../images/evrp/plot_timeline_{example_name}.png", format="png", scale=300/ 25.4)
 
 
 if __name__ == "__main__":
