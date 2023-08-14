17012455fb357e6a724680b33d5fa1a83746c1db
fragkedaki <kleio.fragkedaki@einride.tech>
2023-08-15 02:04:08 +0300

    update: hyperparameter tuning configuration

--- hyperparamer_tuning.py
+++ hyperparamer_tuning.py
@@ -35,12 +35,29 @@ def run(config: dict()):
 
 
 if __name__ == "__main__":
-    N_ITER = 100
-    ray.init(num_cpus=5)
+    N_ITER = 20
+    ray.init(num_cpus=3)
+
+    current_best_params = [
+        {
+            "n_encode_layers": 3,
+            "lr_model": 0.0001,
+            "batch_size": 512,
+            "n_epochs": 50,
+            "optimizer_class": "NAdam",
+        }
+    ]
+
     searcher = HyperOptSearch(
-        space=config, metric="loss", mode="min", n_initial_points=int(N_ITER / 10)
+        space=config,
+        metric="loss",
+        mode="min",
+        n_initial_points=int(N_ITER / 10),
+        points_to_evaluate=current_best_params,
     )
-    algo = ConcurrencyLimiter(searcher, max_concurrent=5)
+
+    algo = ConcurrencyLimiter(searcher, max_concurrent=3)
+
     objective = tune.with_resources(
         tune.with_parameters(run), resources={"cpu": 1, "memory": 400 * 1000000}
     )
@@ -55,6 +72,5 @@ if __name__ == "__main__":
             num_samples=N_ITER,
         ),
     )
-
     results = tuner.fit()
     ray.shutdown()
--- src/utils/hyperparameter_config.py
+++ src/utils/hyperparameter_config.py
@@ -4,7 +4,7 @@ config = {
     "n_encode_layers": hp.randint("n_encode_layers", 3, 5),
     "lr_model": hp.uniform("lr_model", 1e-6, 0.001),
     "batch_size": hp.choice("batch_size", [128, 256, 512]),
-    "n_epochs": hp.randint("epochs", 30, 150),
+    "n_epochs": hp.randint("n_epochs", 30, 150),
     "optimizer_class": hp.choice("optimizer_class", ["Adam", "NAdam", "Adamax"]),
     "hyperparameter_tuning": True,
 }
